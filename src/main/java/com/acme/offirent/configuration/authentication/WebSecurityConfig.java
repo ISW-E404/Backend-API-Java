9dcb1136354bc28de6d4fbd00ee55df47ee01b25
judai <ernesvz@hotmail.com>
2021-06-14 16:13:45 -0500

    feat: modifying cors configuration in webSecurityConfig class

--- src/main/java/com/acme/offirent/configuration/authentication/WebSecurityConfig.java
+++ src/main/java/com/acme/offirent/configuration/authentication/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.csrf().and().cors().disable()
                 .authorizeRequests()
                 .antMatchers("/api/*","/offirent-api-docs","/offirent-api-docs-ui.html","/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated().and()
