41fc4cee5c7f2321ff3d8d97bf67e4faaf0c8cca
judai <ernesvz@hotmail.com>
2021-06-14 12:57:56 -0500

    fix: adding swagger paths at csrf.disable()

--- src/main/java/com/acme/offirent/configuration/authentication/WebSecurityConfig.java
+++ src/main/java/com/acme/offirent/configuration/authentication/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/*").permitAll()
+                .antMatchers("/api/*","/offirent-api-docs","/offirent-api-docs-ui.html").permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint).and()
