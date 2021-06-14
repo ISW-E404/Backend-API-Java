0e2c891d45b395704571b0abce1f8e1318c6c9dd
judai <ernesvz@hotmail.com>
2021-06-14 17:33:27 -0500

    fix: delete swagger.configUrl in application.properties

--- src/main/java/com/acme/offirent/configuration/authentication/WebSecurityConfig.java
+++ src/main/java/com/acme/offirent/configuration/authentication/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/*","/offirent-api-docs","/offirent-api-docs-ui.html","/v3/api-docs","offirent/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+                .antMatchers("/api/*","/offirent-api-docs","/offirent-api-docs-ui.html/*","/v3/api-docs","offirent/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint).and()
