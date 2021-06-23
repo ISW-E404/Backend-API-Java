660324d882accd4fffba14d75fdd97c1ef2f2600
judai <ernesvz@hotmail.com>
2021-06-22 20:16:29 -0500

    fix: replace for querys in methods about prices of offices

--- src/main/java/com/acme/offirent/domain/repository/OfficeRepository.java
+++ src/main/java/com/acme/offirent/domain/repository/OfficeRepository.java
@@ -4,6 +4,7 @@ import com.acme.offirent.domain.model.Office;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.util.List;
@@ -11,8 +12,12 @@ import java.util.Optional;
 
 public interface OfficeRepository extends JpaRepository<Office, Long> {
        Page<Office> findAllByDistrictId (Long districtId, Pageable pageable);
+
+       @Query("select o from Office o where o.price <= ?1")
        Optional<Page<Office>> findByPriceLessThanEqual (float price, Pageable pageable);
        List<Office> findAllByAccountId(Long accountId);
        Page<Office> findAllByAccountId(Long accountId,Pageable pageable);
+
+       @Query("select o from Office o where o.price between ?1 and ?2")
        Optional<Page<Office>> findAllOfficesByPriceLessThanEqualAndPriceGreaterThanEqual(float price1, float price2, Pageable pageable);
 }
